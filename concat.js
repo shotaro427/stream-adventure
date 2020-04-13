7ce237dc26785426fd52a98b996a75fc995f6dc1
shotaro427 <toshinobu.s111@gmail.com>
2020-04-13 13:02:23 +0900

    learned lesson8

--- concat.js
+++ concat.js
@@ -1,15 +1,3 @@
-var fs = require('fs')
-var http = require('http')
-var through = require('through2')
-
-var server = http.createServer(function (req, res) {
-    if (req.method == 'POST') {
-        req.pipe(through(function (line, _, next) {
-            this.push(line.toString().toUpperCase())
-            next()
-        })).pipe(res)
-    }
-    else res.end("send me a POST\n")
-})
-
-server.listen(Number(process.argv[2]))
+var request = require('request')
+var r = request.post("http://localhost:8099")
+process.stdin.pipe(r).pipe(process.stdout)
